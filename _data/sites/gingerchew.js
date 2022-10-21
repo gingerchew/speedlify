d9e24e23f31db4cdc45c19a7840eb3e9837b623f
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-10-21 19:10:10 -0400

    Get's rid of special characters in file name

--- _data/sites/gingerchew.js
+++ _data/sites/gingerchew.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: "Ginger's Super Ultra Fast Sites :)", // optional, falls back to object key
+	name: "My Super Ultra Fast Sites", // optional, falls back to object key
 	description: "Holding myself accountable to keep my sites fast, accessible, good at SEO (I guess), and following best practices. Going for all perfect scores!",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
