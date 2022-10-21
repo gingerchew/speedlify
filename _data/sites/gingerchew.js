ff85a42b7a456c92c3084cc5aaf2fee41cc222ee
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-10-21 19:06:26 -0400

    Changes the URLs to be for my sites
    Makes sense to not just be for one site, y'know

---
+++ _data/sites/gingerchew.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Ginger's Super Ultra Fast Sites :)", // optional, falls back to object key
+	description: "Holding myself accountable to keep my sites fast, accessible, good at SEO (I guess), and following best practices. Going for all perfect scores!",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://resourcesand.help/",
+		"https://ginger.wtf/"
+	]
+};
