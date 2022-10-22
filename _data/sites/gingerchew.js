51c2f4e0faa4bdbff17ffe83e20c47084ddcc6c7
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-10-21 20:37:06 -0400

    Changes description
    Description was not fitting right on the site, now it looks better

--- _data/sites/gingerchew.js
+++ _data/sites/gingerchew.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "My Super Ultra Fast Sites", // optional, falls back to object key
-	description: "Holding myself accountable to keep my sites fast, accessible, good at SEO (I guess), and following best practices. Going for all perfect scores!",
+	description: "Sites made by Ginger",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
